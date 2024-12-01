87a200d320f6c5b64731ac72c7281f792456aef3
Lucy Hudson <lucyhudson03@aol.com>
2024-12-01 14:57:16 +0000

    Adding extra comments and creating a saved function for the logistic growth curve.

---
+++ Functions/Logistic Function.R
@@ -0,0 +1,7 @@
+logistic_fun <- function(t) {
+  
+  N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
+  
+  return(N) #give us the N value out of the function
+  
+}
--- fit_linear_model.R
+++ fit_linear_model.R
@@ -7,9 +7,17 @@ growth_data <- read.csv("experiment.csv")
 #Case 1. K >> N0, t is small
 
 data_subset1 <- growth_data |> filter(t< 1500) |> mutate(N_log = log(N))
+#creating a new data set where you only have the dat up to t=1500 and also
+#you add an extra column with the Log of the populations.
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
+#The intercept of this is the starting population, however as it is logged you 
+#need to unlog it.
+
+exp(7.185544) #unlogging the intercept of Datasubset 1 to find N0
+
+#N0 = 1320.207
 
 #Case 2. N(t) = K
 
@@ -18,4 +26,3 @@ data_subset2 <- growth_data |> filter(t>2500)
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
 
-exp(7.185544) #unlogging the intercept od Datasubset 1 to find N0
