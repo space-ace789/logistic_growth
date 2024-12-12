ce9f579309b2620dc5ca803a63600834efc7ada8
space-ace789 <wadh6644@ox.ac.uk>
2024-12-12 23:39:25 +0000

    Question 3 more complete

--- comparing_growth_models.R
+++ comparing_growth_models.R
@@ -15,11 +15,11 @@ K <- 6.00e+10
 ggplot(aes(t,N), data = growth_data) +
   geom_function(fun=logistic_fun, aes(color= "Logistic")) +
   geom_function(fun=exponential_fun, aes(color= "Exponential")) +
-  labs(y = "Log(Population Size)", x = "Time", title = "Exponential Model of Bacterial Growth compared to a Logistic Model of Growth") +
+  labs(y = "Log Transformed Population Size", x = "Time", title = "Exponential Model of Bacterial Growth compared to a Logistic Model of Growth") +
   scale_y_continuous(trans='log10') +
   scale_color_manual(name='Growth Model',
                      breaks=c('Exponential', 'Logistic'),
                      values=c('Exponential'='blue', 'Logistic'='red')) +
-  theme(plot.title = element_text(size = 9, hjust = 0.5), legend.position = c(0.3, 0.70))
+  theme(plot.title = element_text(size = 10, hjust = 0.5), legend.position = c(0.2, 0.750))
 
 
